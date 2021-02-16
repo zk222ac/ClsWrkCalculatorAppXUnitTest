a162bfed89c7f326db5d73b9afc6adf310defee4
Zuhair Haroon Khan <zk222ac@gmail.com>
2021-02-16 11:31:42 +0100

    Add project files.

---
+++ CalculatorProductionCode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClsWrkCalculatorAppXUnitTest
+{
+   public class CalculatorProductionCode
+    {
+        // Addition Method
+        public static int Addition(int a , int b)
+        {
+            return a + b;
+        }
+
+        // Substraction Method 
+        public static int Substraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        // Multiplication Method 
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        // Division Method 
+        public static int Division(int a, int b)
+        {
+            return a / b;
+        }
+
+        // Modulation Method 
+        public static int Modulation(int a, int b)
+        {
+            return a % b;
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClsWrkCalculatorAppXUnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
